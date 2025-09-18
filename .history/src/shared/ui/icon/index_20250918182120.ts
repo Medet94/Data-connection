7854cd513bf7b37cd43fee8da5b39b07c5fc4159
Medet <medet.berikbaev.94@gmail.com>
2025-09-18 18:27:55 +0500

    feat(data-connection) api added

---
+++ .history/src/shared/ui/icon/index_20250918182120.ts
@@ -0,0 +1,2 @@
+export { BluePrintIcon } from './BluePrintIcon';
+export type { BluePrintIconName } from './BluePrintIcon';
---
+++ src/shared/ui/Loader.tsx
@@ -0,0 +1,7 @@
+import { Loader as MantineLoader, Center } from '@mantine/core';
+
+export const Loader = () => (
+  <Center p="xl">
+    <MantineLoader size="lg" />
+  </Center>
+);
